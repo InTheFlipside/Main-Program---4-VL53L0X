efc0329a627f026f57c845cc49967d7eeac45b9a
idkhbtfm <26010778+InTheFlipside@users.noreply.github.com>
2019-01-17 16:29:31 +0100

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,33 @@
+#include <VL53L0X.h>
+#include <Wire.h>
+
+#define XSHUT_pin 3
+
+#define Sensor_newAddress 29
+
+VL53L0X Sensor;
+void setup() {
+   Serial.begin(9600);
+  // put your setup code here, to run once:
+ pinMode(XSHUT_pin, OUTPUT);
+
+ Wire.begin();
+
+  pinMode(XSHUT_pin, INPUT);
+  delay(10);
+  Sensor.setAddress(Sensor_newAddress);
+  Sensor.init();
+  Sensor.startContinuous();
+ 
+}
+
+void loop() {
+
+Serial.print(Sensor.readRangeContinuousMillimeters());
+
+Serial.print("                           ");
+
+  
+  // put your main code here, to run repeatedly:
+
+}
